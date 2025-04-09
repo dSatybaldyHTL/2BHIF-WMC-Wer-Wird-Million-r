cbcfbc8d359430df2515ee692027427fd4513a86
s-elgitHTL <s.elgit@students.htl-leonding.ac.at>
2025-04-09 14:56:37 +0200

    added code in index.js

--- JS-Code/index.js
+++ JS-Code/index.js
@@ -1,15 +1,18 @@
 
-fetch('https://localhost:3000/questions')
-.then(response => response.json())
-.then(data => {
-    data.forEach((question) => {
-
-    })
-})
-
 function checkAnswer(userAnswer, answer) {
     if (userAnswer === answer) {
         return true;
     }
     return false;
 }
+
+fetch('https://localhost:3000/questions')
+.then(response => response.json())
+.then(data => {
+        let questionCounter = 0;
+
+        function loadQuestion(question) {
+            document.getElementById('frageText').textContent = question.frage;
+            const antwortenContainer = document.getElementById('antworten-container');
+            antwortenContainer.innerHTML = '';
+})
