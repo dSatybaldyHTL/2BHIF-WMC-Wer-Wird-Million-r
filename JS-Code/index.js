cd4ff928a634c3a0a1e5cd56091f9812960fb682
s-elgitHTL <s.elgit@students.htl-leonding.ac.at>
2025-04-09 15:15:15 +0200

    added code in index.js

--- JS-Code/index.js
+++ JS-Code/index.js
@@ -15,4 +15,51 @@ fetch('https://localhost:3000/questions')
             document.getElementById('frageText').textContent = question.frage;
             const antwortenContainer = document.getElementById('antworten-container');
             antwortenContainer.innerHTML = '';
+
+            question.antworten.forEach(item => {
+                const button = document.createElement('button');
+                button.textContent = item;
+                button.classList.add('antworten-button');
+                button.onclick = () => {
+                    const isCorrect = checkAnswer(item, question.rightAnswer);
+
+                    if (isCorrect) {
+                        button.style.backgroundColor = 'green';
+                    }
+                    else
+                        button.style.backgroundColor = 'red';
+
+                    const buttons = document.querySelectorAll('.antwort-button');
+                    buttons.forEach((button) => {
+                        if (button.textContent === question.rightAnswer) {
+                            button.style.backgroundColor = 'green';
+                        }
+                    });
+                    disableButtons();
+
+                    // "Nächste Frage"-Button anzeigen
+                    document.getElementById('nextButton').style.display = 'block';
+                };
+                antwortenContainer.appendChild(button);
+
+            });
+        }
+    function disableButtons() {
+        const buttons = document.querySelectorAll('.antwortButton');
+        buttons.forEach(btn => btn.disabled = true);
+    }
+
+    document.getElementById('nextButton').onclick = () => {
+        questionCounter++;
+        if (questionCounter < data.length) {
+            loadQuestion(data[questionCounter]);
+            document.getElementById('nextButton').style.display = 'none';
+        } else {
+            document.getElementById('frageText').textContent = "Quiz beendet!";
+            document.getElementById('antworten-container').innerHTML = '';
+            document.getElementById('nextButton').style.display = 'none';
+        }
+    }
+    loadQuestion(data[questionCounter]);
 })
+.catch(error => console.error("Fehler beim Laden:", error));
